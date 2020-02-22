e5cbabff39b2d82e1b4adae7272561b9770fb792
foxhoundsk <foxhoundsk.tw@gmail.com>
2020-02-23 00:11:54 +0800

    Rewrite kthread-based operations with CMWQ
    The CMWQ impl has merely the same behavior compare with kthread-ver
    daemon, except that when a connection is still alive at removal of
    the module, kthread based daemon will simply proceed with the module
    removal, but crash the machine once that any of existing connection
    being closed, whereas CMWQ based one will simply closes all
    connections.
    
    The benefit of using the CMWQ is, we're no longer need to create
    kthread each time a request arrived, we just simply queue the task
    into it. this saves us bunch of time on kthread creation. Moreover,
    one kthread of the CMWQ may processes multiple tasks we queued into,
    which means we can get better performance due to cache locality.

--- echo_server.h
+++ echo_server.h
@@ -1,12 +1,25 @@
 #ifndef ECHO_SERVER_H
 #define ECHO_SERVER_H
 
+#include <linux/module.h>
+#include <linux/workqueue.h>
 #include <net/sock.h>
 
 #define MODULE_NAME "fastecho"
 
 struct echo_server_param {
     struct socket *listen_sock;
+};
+
+struct echo_service {
+    bool is_stopped;
+    struct list_head worker;
+};
+
+struct fastecho {
+    struct socket *sock;
+    struct list_head list;
+    struct work_struct fastecho_work;
 };
 
 extern int echo_server_daemon(void *);
